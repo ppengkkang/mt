1b06269441b6da5f610ba6f0f99b32a6deff5368
ppengkkang <45534469@qq.com>
2016-11-24 19:01:19 +0800

    dubbo support

--- src/main/java/com/brightd/controller/UserController.java
+++ src/main/java/com/brightd/controller/UserController.java
@@ -3,6 +3,7 @@ package com.brightd.controller;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
+import com.demo.service.DemoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +16,20 @@ import com.brightd.service.IUserService;
 public class UserController {
 	@Resource
 	private IUserService userService;
+
+    @Resource(name = "demoService")
+    private DemoService demoService;
 	
 	@RequestMapping("/showUser")
 	public String toIndex(HttpServletRequest request,Model model){
 		//int userId = Integer.parseInt(request.getParameter("id"));
         int userId = 1;
-		User user = this.userService.getUserById(userId);
+		//User user = this.userService.getUserById(userId);
+        User user = new User();
+        String str = demoService.sayHello("tom&jerry");
+        user.setUserName(str);
+        user.setAge(10);
+        user.setId(1);
 		model.addAttribute("user", user);
 		return "showUser";
 	}
